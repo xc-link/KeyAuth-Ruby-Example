202d395f7b70cb0a9e353d33c8c57bc1fcac5376
xc-link <cai_liyong@qq.com>
2024-10-24 06:53:15 +0800

    Create AJX.rb

---
+++ AJX.rb
@@ -0,0 +1,6 @@
+KeyAuth.new.Api(
+    "AJX", # App name - VaultCord.com FREE Discord backup bot for members & your entire server saved from terms and nukes!
+    "o0dHuOg0Bv", # Account ID
+    "f8f4af5e0759edfb5c948e2e1b3efaa36e23c5543f16a94288fc1785d3e4e3b1", # Encryption key, keep hidden and and protect this string in your code!
+    "1.0" # Application version. Used for automatic downloads see video here https://www.youtube.com/watch?v=kW195PLCBKs
+)
